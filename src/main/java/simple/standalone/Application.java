a3a3f24c03aafc26073eb3066726f54b1b9da0a5
anoissi <NRx2x2nm>
2017-12-23 19:35:54 +0100

    spring boot standalone application sample

---
+++ src/main/java/simple/standalone/Application.java
@@ -0,0 +1,28 @@
+package simple.standalone;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class Application {
+    private static Logger logger = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) throws Exception {
+        ConfigurableApplicationContext ctx =
+                SpringApplication.run(Application.class, args);
+        assert (ctx != null);
+        logger.info("The beans you were looking for:");
+        // listing all bean definition    names
+        Arrays.stream(ctx.getBeanDefinitionNames()).forEach(logger::info);
+        HelloWorld hw = ctx.getBean(HelloWorld.class);
+        hw.sayHi();
+        System.in.read();
+        ctx.close();
+    }
+}
