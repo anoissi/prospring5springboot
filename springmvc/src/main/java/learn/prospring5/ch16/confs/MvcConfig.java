7bfcd4d3610713a78e2280f2582baf4267cb84ea
anoissi <NRx2x2nm>
2018-01-13 16:38:30 +0100

    spring mvc + spring jpa + spring boot

---
+++ springmvc/src/main/java/learn/prospring5/ch16/confs/MvcConfig.java
@@ -0,0 +1,26 @@
+package learn.prospring5.ch16.confs;
+
+import learn.prospring5.ch16.utils.DateFormatter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/index").setViewName("index");
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/login").setViewName("login");
+    }
+    @Override
+    public void addFormatters(FormatterRegistry formatterRegistry) {
+        formatterRegistry.addFormatter(dateFormatter());
+    }
+    @Bean
+    public DateFormatter dateFormatter() {
+        return new DateFormatter();
+    }
+}
--- springmvc/src/main/java/learn/prospring5/ch16/confs/WebSecurityConfig.java
+++ springmvc/src/main/java/learn/prospring5/ch16/confs/WebSecurityConfig.java
@@ -5,11 +5,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.
-        WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter  {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password("{noop}user").roles("USER");
+    }
+
     @Override
     protected void configure(HttpSecurity http)  throws Exception {
             http.authorizeRequests()
@@ -23,12 +29,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter  {
                 .and()
                 .logout()
                 .permitAll();
-    }
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .withUser("user").password("user").roles("USER");
     }
 }
