f6d06ff681eb66f1425c886bd2b83ca99c73e88a
anoissi <NRx2x2nm>
2018-01-14 22:44:44 +0100

    exercise : internationalization enabling - spring boot

--- springmvc/src/main/java/learn/prospring5/ch16/confs/MvcConfig.java
+++ springmvc/src/main/java/learn/prospring5/ch16/confs/MvcConfig.java
@@ -3,9 +3,15 @@ package learn.prospring5.ch16.confs;
 import learn.prospring5.ch16.utils.DateFormatter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.util.Locale;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
@@ -22,5 +28,35 @@ public class MvcConfig implements WebMvcConfigurer {
     @Bean
     public DateFormatter dateFormatter() {
         return new DateFormatter();
+    }
+
+    @Bean
+    ReloadableResourceBundleMessageSource messageSource()  {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasenames("classpath:/i18n/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setFallbackToSystemLocale(false);
+        return  messageSource;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Bean
+    LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+        interceptor.setParamName("lang");
+        return interceptor;
+    }
+
+    @Bean
+    CookieLocaleResolver localeResolver() {
+        CookieLocaleResolver cookieLocaleResolver = new CookieLocaleResolver();
+        cookieLocaleResolver.setDefaultLocale(Locale.ENGLISH);
+        cookieLocaleResolver.setCookieMaxAge(3600);
+        cookieLocaleResolver.setCookieName("locale");
+        return  cookieLocaleResolver;
     }
 }
